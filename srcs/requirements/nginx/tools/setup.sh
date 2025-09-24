278bbc38f58ba3f39ea5567e3f2ecfc56dabf4da
francois <fpetit@debian.fpetit.42.fr>
2025-09-24 18:27:59 +0200

    running nginx

--- srcs/requirements/nginx/tools/setup.sh
+++ srcs/requirements/nginx/tools/setup.sh
@@ -1,9 +1,17 @@
+#!/bin/sh
+
 if [ ! -f /etc/nginx/certs/mycert.crt ]; then
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
     -keyout /etc/nginx/certs/mycert.key \
     -out /etc/nginx/certs/mycert.crt \
-    -subj "/CN=FR/ST=NA/L=Angouleme/O=42/OU=42Angouleme/CN=xxx.42.fr/UID=xxx"
+    -subj "/CN=FR/ST=NA/L=Angouleme/O=42/OU=42Angouleme/CN=fpetit.42.fr/UID=fpetit"
 fi
+
+# check permissions
+
+chmod 600 /etc/nginx/certs/mycert.key
+
+echo "Setup complete ... starting nginx ..."
 
 # execute final CMD
 exec "$@"
