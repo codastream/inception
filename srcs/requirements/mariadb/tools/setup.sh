63adabde0ef9e2e2424e098dd0ae03da54dd6dc8
francois <fpetit@debian.fpetit.42.fr>
2025-09-27 21:57:13 +0200

    wip

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -9,27 +9,56 @@ SQL_ROOT_PASSWORD=$(cat /run/secrets/sql_root_password)
 # -o pipefail error if an intermediate command fails in a pipe
 set -euo pipefail
 
-# start in background
-mariadbd-safe --skip-networking --user=mysql
+mkdir -p /run/mysqld
+chown -R mysql:mysql /var/lib/mysql /run/mysqld
+chmod 754 /run/mysqld
 
-# wait
-until mysqladmin ping >/dev/null 2>&1; do
-   echo "Waiting for MariaDB to start..."
-   sleep 2
-done
+if [ ! -d /var/lib/mysql/mysql ]; then
+    echo "Installing MariaDB..."
 
-# create DB
-mariadb -uroot <<-EOSQL
-  CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\` CHARACTER SET utf8 COLLATE utf8_general_ci;
-  CREATE USER IF NOT EXISTS \`${SQL_ADMIN}\`@'localhost' IDENTIFIED BY '${SQL_ADMIN_PASSWORD}';
-  CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_USER_PASSWORD}';
-  GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_ADMIN}\`@'%' IDENTIFIED BY ${SQL_ADMIN_PASSWORD};
-  ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
-  FLUSH PRIVILEGES;
+    # mariadb-secure-installation
+    mariadb-install-db \
+        --user=mysql \
+        --datadir=/var/lib/mysql \
+        --server-debug \
+        --verbose \
+        --skip-test-db \
+        --auth-root-authentication-method=normal
+
+    chown -R mysql:mysql /var/lib/mysql /run/mysqld
+
+    # start in background
+    # --skip-networking : disable TCP/IP only accepts connections from local machine
+    # $! captures pid of background process
+    mariadbd-safe --skip-networking --user=mysql --socket=/run/mysqld/mysqld.sock & pid="$!"
+    # wait
+    echo "Waiting for MariaDB to start..."
+    until mariadb-admin ping --socket=/run/mysqld/mysqld.sock --silent; do
+        sleep 2
+    done
+
+    # secure root user
+    # create users from any host @ '%'
+    echo "Initializing db and users..."
+    mariadb -uroot <<-EOSQL
+    CREATE DATABASE IF NOT EXISTS '${SQL_DATABASE}' CHARACTER SET utf8 COLLATE utf8_general_ci;
+    CREATE USER IF NOT EXISTS '${SQL_ADMIN}'@'%' IDENTIFIED BY '${SQL_ADMIN_PASSWORD}';
+    CREATE USER IF NOT EXISTS '${SQL_USER}'@'%' IDENTIFIED BY '${SQL_USER_PASSWORD}';
+    GRANT ALL PRIVILEGES ON '${SQL_DATABASE}'.* TO '${SQL_ADMIN}'@'%' IDENTIFIED BY '${SQL_ADMIN_PASSWORD}';
+    ALTER USER 'root'@'%' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
+    FLUSH PRIVILEGES;
 EOSQL
 
-# refresh
-mysqladmin -uroot -p'${SQL_ROOT_PASSWORD}' shutdown
+    echo "Shutting down MariaDB temporary server ..."
+    # refresh
+    mariadb-admin -uroot -p"${SQL_ROOT_PASSWORD}" --socket=/run/mysqld/mysqld.sock shutdown
+    # waiting for clean shutdown
+    wait $pid
 
+fi
+
+tail -n50 /var/lib/mysql/*.err
+
+echo "Starting MariaDB ..."
 exec su-exec mysql "$@"
 
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -4,44 +4,69 @@ WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
 WP_USER_PASSWORD=$(cat /run/secrets/wp_user_password)
 SQL_USER_PASSWORD=$(cat /run/secrets/sql_user_password)
 
-@echo "===Wordpress setup==="
-@echo "Downloading..."
-wp core download --path=/var/www/wordpress --allow-root
+WP_PATH="/var/www/html"
+WP_CONFIG="$WP_PATH/wp_config.php"
 
-until mariadb -h localhost -u"$SQL_USER" -p"$SQL_USER_PASSWORD" -e "SELECT 1" >/dev/null 2>&1; do
-    echo "[INFO] Waiting for MariaDB..."
-    sleep 2
+echo "===Wordpress setup==="
+
+mkdir -p "$WP_PATH"
+cd "$WP_PATH"
+
+if [ ! -f "$WP_PATH/wp-settings.php" ]; then
+    echo "Downloading..."
+    wp core download --path=/var/www/wordpress --allow-root
+fi
+
+until mariadb -h mariadb -u"$SQL_USER" -p"$SQL_USER_PASSWORD" \
+    -e "SELECT 1" >/dev/null 2>&1; do
+    echo "Waiting for MariaDB..."
+    sleep 3
 done
 
-@echo "Configuring db..."
-wp config create \
-  --allow-root \
-  --dbname=$SQL_DATABASE \
-  --dbuser=$SQL_USER \
-  --dbpass=$SQL_USER_PASSWORD \
-  --dbhost=mariadb:3306 \
-  --dbcharset="utf8" \
-  --dbcollate="utf8_general_ci" \
-  --path='/var/www/wordpress'
+echo "MariaDB ready!"
 
-@echo "Url and admin setup..."
-wp core install \
-  --url=$WP_URL \
-  --title="Inception"\
-  --admin_user=$WP_ADMIN_LOGIN \
-  --admin_password=$WP_ADMIN_PASSWORD \
-  --admin_email=$WP_ADMIN_EMAIL \
-  --skip-email
+if [ ! -f "$WP_CONFIG" ]; then
+    echo "Configuring db..."
+    wp config create \
+      --dbname=$SQL_DATABASE \
+      --dbuser=$SQL_USER \
+      --dbpass=$SQL_USER_PASSWORD \
+      --dbhost="mariadb" \
+      #--dbcharset="utf8" \
+      #--dbcollate="utf8_general_ci" \
+      --path="$WP_PATH" \
+      --allow-root
+fi
 
-@echo "Adding new user..."
-wp user create \
-  $WP_USER_LOGIN \
-  $WP_USER_EMAIL \
-  --user_pass=$WP_USER_PASSWORD
-  --role=author
-  --porcelain
+if [ ! wp core is-installed --path="$WP_PATH" --allow-root 2>/dev/null; ]; then
+    echo "Installing WordPress..."
+    wp core install \
+      --path="$WP_PATH" \
+      --url=$WP_URL \
+      --title="Inception"\
+      --admin_user=$WP_ADMIN_LOGIN \
+      --admin_password=$WP_ADMIN_PASSWORD \
+      --admin_email=$WP_ADMIN_EMAIL \
+      --skip-email \
+      --allow-root
+fi
 
-@echo "Choosing theme..."
+if [ -n "${WP_USER:-}" ] && [ -n "${WP_USER_EMAIL:-}" ]; then
+    echo "Adding new user..."
+    wp user create \
+        $WP_USER_LOGIN \
+        $WP_USER_EMAIL \
+        --user_pass=$WP_USER_PASSWORD
+        --role=author \
+        --path="$WP_PATH" \
+        --porcelain \
+        --allow-root
+fi
+
+echo "Choosing theme..."
 wp theme install twentytwentyfour --activate
+
+chown -R www:www "$WP_PATH"
+chmod -R 755 "$WP_PATH"
 
 exec "$@"
