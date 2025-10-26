a7d790f4c7c4ca452b1625af0e6bf958f46de86b
fp <fp@42>
2025-10-26 22:21:10 +0100

    [wip]

--- srcs/requirements/bonus/ftp/tools/setup.sh
+++ srcs/requirements/bonus/ftp/tools/setup.sh
@@ -1,50 +1,83 @@
-#!/bin/sh
-
-FTP_USER_HOME="/var/www/wordpress/"
+#!/bin/bash
 
 set -euxo pipefail
-
 FTP_PASS=$(cat /run/secrets/ftp_user_password)
+FTP_USER=ftpuser
 
-mkdir -p /var/log/vsftpd /etc/vsftpd /var/www/wordpress/wp-content/uploads
-
-# adding www  user and group
-if ! getent group www >/dev/null 2>&1; then
-  addgroup -g 1001    www
-fi
-if ! getent passwd www >/dev/null 2>&1; then
-  adduser  -u 1001    -D -G www -h /var/www               -s /sbin/nologin www
-fi
-
-# adding secure user for vsftpd
-if ! getent passwd ftpsecure >/dev/null 2>&1; then
-  adduser  -u 998     -D -G www -h /var/empty             -s /sbin/nologin -H ftpsecure
-fi
-
-# addding FTP user with a fixed UID/GID to avoid permission issues with wordpress files
-if ! getent passwd ftpuser >/dev/null 2>&1; then
-  adduser  -u 999     -D -G www -h $FTP_USER_HOME         -s /sbin/nologin ftpuser
-  echo "$FTP_USER:$FTP_PASS" | chpasswd
-fi
-
-if [ ! -f /etc/vsftpd/vsftpd.user_list ]; then
-  echo "$FTP_USER" | tee -a /etc/vsftpd/vsftpd.user_list
-  chmod 644 /etc/vsftpd/vsftpd.user_list
-fi
+# crate secure chroot directory
+mkdir -p /var/run/vsftpd/empty
+mkdir -p /var/log/vsftpd/
 
 until [ -d "/var/www/wordpress/wp-content" ] && [ -f "/var/www/wordpress/wp-config.php" ]; do
   echo "Waiting for WordPress..."
   sleep 2
 done
 
-# Set ownership for WordPress files
-chown -R www:www /var/www/wordpress
-chmod -R 755 /var/www/wordpress
+# Create www group with GID 1001 if it doesn't exist
+if ! getent group www > /dev/null 2>&1; then
+  groupadd -g 1001 www
+fi
 
-# Allow FTP user to write to wp-content/uploads
-chown -R ftpuser:www /var/www/wordpress/wp-content/uploads
-chmod -R 775 /var/www/wordpress/wp-content/uploads
+if [ ! -f "/etc/vsftpd.conf.bak" ]; then
 
-echo "Running"
+  mkdir -p /var/run/vsftpd/empty
+	cp /etc/vsftpd.conf /etc/vsftpd.conf.bak
+	mv /var/www/vsftpd.conf /etc/vsftpd.conf
 
-exec /usr/sbin/vsftpd /etc/vsftpd/vsftpd.conf
+	adduser --disabled-password --gecos "" \
+          --home /var/www/wordpress/wp-content/uploads \
+          --shell /bin/bash \
+          --ingroup www \
+          $FTP_USER 
+
+	echo "$FTP_USER:$FTP_PASS" | /usr/sbin/chpasswd &> /dev/null
+
+	chown -R $FTP_USER:www /var/www/wordpress/wp-content/uploads
+  chmod -R 2775 /var/www/wordpress/wp-content/uploads
+
+	echo $FTP_USER >> /etc/vsftpd.userlist
+
+fi
+
+
+
+# # Create www user with UID 1001 if it doesn't exist
+# if ! id -u www > /dev/null 2>&1; then
+#   useradd -u 1001 -g www -d /var/www -s /sbin/nologin www
+# fi
+
+# # Create FTP user with UID 999 and add to www group (GID 1001)
+# if ! id -u $FTP_USER > /dev/null 2>&1; then
+#   mkdir -p /var/www/wordpress/wp-content/uploads
+#   useradd -u 999 -g www -d /var/www/wordpress/wp-content/uploads -s /bin/bash $FTP_USER
+#   echo "$FTP_USER:$FTP_PASS" | chpasswd
+#   echo "$FTP_USER" | tee -a /etc/vsftpd.userlist
+# fi
+
+# chgrp -R www /var/www/wordpress/wp-content/uploads
+# chmod -R 2775 /var/www/wordpress/wp-content/uploads /var/www/wordpress/wp-content/uploads /var/www/wordpress/wp-content/uploads/2025 /var/www/wordpress/wp-content/uploads/2025/10
+# find /var/www/wordpress/wp-content/uploads -type d -exec chmod 2775 {} +
+# find /var/www/wordpress/wp-content/uploads -type f -exec chmod 664 {} +
+
+# sed -i -r "s/#write_enable=YES/write_enable=YES/1"   /etc/vsftpd.conf
+# sed -i -r "s/#chroot_local_user=YES/chroot_local_user=YES/1"   /etc/vsftpd.conf
+
+# echo "
+# write_enable=YES
+# chroot_local_user=YES
+# local_enable=YES
+# allow_writeable_chroot=YES
+# pasv_enable=YES
+# local_root=/var/www/wordpress/wp-content/uploads
+# pasv_min_port=21100
+# pasv_max_port=21110
+# userlist_enable=YES
+# userlist_deny=NO
+# background=NO
+# xferlog_enable=YES
+# log_ftp_protocol=YES
+# vsftpd_log_file=/var/log/vsftpd/vsftpd.log
+# xferlog_file=/var/log/vsftpd/xferlog
+# userlist_file=/etc/vsftpd.userlist" >> /etc/vsftpd.conf
+
+exec /usr/sbin/vsftpd /etc/vsftpd.conf
